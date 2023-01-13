774b74fc910ba0909c960347b1043bb1f4dd88e1
bc8c <35796622+bc8c@users.noreply.github.com>
2023-01-13 21:42:36 +0900

    add dynamicClass Ex

--- firstproj/src/App.java
+++ firstproj/src/App.java
@@ -1,11 +1,12 @@
 
 import C01.S03_Class.AttendanceBook;
+import C01.S04_GenericClass.DynamicAttendanceBook;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-        testAttendanceBook();        
+        DynamicAttendanceBook();        
 
 
     }
@@ -23,7 +24,33 @@ public class App {
         // ab.deleteStudnet(4);
         ab.deleteStudnet("박감사");
         ab.printStudent();
-        System.out.println(ab.count);
+        System.out.println(ab.count);        
+    }
+
+    static void DynamicAttendanceBook() {
+        DynamicAttendanceBook<String> ab1 = new DynamicAttendanceBook<String>(6);
+        ab1.insertStudent("홍길동");
+        ab1.insertStudent("김친구");
+        ab1.insertStudent("박감사");
+        ab1.insertStudent("이소리");
+        ab1.insertStudent("김둥실");
+        ab1.insertStudent("최자바");
+
+        ab1.printStudent();
+        System.out.println("---------------------------------------------");
+        ab1.deleteStudnet("박감사");
+        ab1.printStudent();
+
+        DynamicAttendanceBook<Integer> ab2 = new DynamicAttendanceBook<Integer>(6);
+        ab2.insertStudent(85);
+        ab2.insertStudent(97);
+        ab2.insertStudent(65);
+        ab2.printStudent();
+        System.out.println("---------------------------------------------");
+        Integer i = Integer.valueOf(97);
+        ab2.deleteStudnet(i);
+        ab2.printStudent();
+       
         
     }
 }
--- firstproj/src/C01/S04_GenericClass/DynamicAttendanceBook.java
+++ firstproj/src/C01/S04_GenericClass/DynamicAttendanceBook.java
@@ -1,22 +1,21 @@
-package C01.S03_Class;
+package C01.S04_GenericClass;
 
-
-public class AttendanceBook {
-    private String[] students;
+public class DynamicAttendanceBook<T> {
+    private T[] students;
     public int count;
 
-    public AttendanceBook(int size) { // 생성자
-                students = new String[size];
-                count = 0;
+    public DynamicAttendanceBook(int size) { // 생성자
+        students =  (T[])new Object[size];
+        count = 0;
     }
 
-    public int insertStudent(String name) { // 입력
+    public int insertStudent(T name) { // 입력
         if(students.length <= count){
             System.out.println("Could not insert : not enough space in this attendancebook");
             return 0;
         } else {
             students[count] = name;
-            count++;
+            count++;            
             System.out.printf("The inserted student index is %d\n", count);
             return count;
         }
@@ -24,7 +23,7 @@ public class AttendanceBook {
 
     public void printStudent() { // 출력
         for (int i = 0; i < count; i++) {
-            System.out.printf("%d:%s\n", i+1, students[i]);
+            System.out.printf("%d:%s\n", i+1, students[i].toString());
         }        
     }
 
@@ -37,7 +36,7 @@ public class AttendanceBook {
         count--;
     }
 
-    public void deleteStudnet(String name) { // 삭제 (name)
+    public void deleteStudnet(T name) { // 삭제 (name)
         for (int i = 0; i < count; i++) {
             if (students[i]==name) {
                 deleteStudnet(i+1);
